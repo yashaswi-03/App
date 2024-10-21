3090ee0cd8460224b5b4a748f25cab25c8ff857f
bogi <yashaswiboga@gmail.com>
2024-10-21 10:02:30 +0530

    App

---
+++ App.js
@@ -0,0 +1,9 @@
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Docker!');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port 3000');
+});
